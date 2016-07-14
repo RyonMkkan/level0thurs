bddde4ad28e641b881438689a8e991f78af8f108
Ryon <league@Mac-mini-2.local>
2016-07-13 17:33:22 -0700

    level0thur
    level0thur

--- src/org/teachingextensions/logo/chooseYourOwnAd.java
+++ src/org/teachingextensions/logo/chooseYourOwnAd.java
@@ -5,7 +5,9 @@ import javax.swing.JOptionPane;
 public class chooseYourOwnAd {
 	public static void main(String[] args) {
 	String answer=JOptionPane.showInputDialog("There is 20hrs left before the earth is invaded by aliens. do you A) prepare to defend yourself or B) prepare to attack ");
-	
+	if (answer.equals("A")) {
+		JOptionPane.show"You prepare to defend yourself but now you only have 3 hours left. you see a little boy  "
+	}
 	}
 
 }
